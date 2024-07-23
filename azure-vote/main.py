0b73ebf987d0d5149413e5930d28591ff00fce0a
CUONG Nguyen Hung <nhcuongit@gmail.com>
2024-07-23 22:44:09 +0700

    Update code

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -105,11 +105,11 @@ def index():
             vote1 = r.get(button1).decode('utf-8')
             properties = {'custom_dimensions': {'Cats Vote': vote1}}
             # TODO: use logger object to log cat vote
-            logger.info(f"{button1} voted.", extra=properties)
+            logger.info(f"{button1} Vote")
 
             vote2 = r.get(button2).decode('utf-8')
             properties = {'custom_dimensions': {'Dogs Vote': vote2}}
-            logger.info(f"{button2} voted.", extra=properties)
+            logger.info(f"{button2} Vote")
             # TODO: use logger object to log dog vote
 
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
